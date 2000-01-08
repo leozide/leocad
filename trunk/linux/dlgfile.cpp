c884706a1cfcc803a84f8e7d9ddd1683fe1080c5
leo <leo@c7d43263-9d01-0410-8a33-9dba5d9f93d6>
2000-01-08 17:51:32 +0000

    Small fixes

---
+++ trunk/linux/dlgfile.cpp
@@ -0,0 +1,23 @@
+//
+// This file holds all the dialogs that are called
+// from the 'File' submenu:
+//
+// - Piece Library Manager
+//
+
+#include <gtk/gtk.h>
+
+// =========================================================
+// Piece Library Manager
+
+
+
+
+
+
+
+
+
+
+
+
--- trunk/linux/module.mk
+++ trunk/linux/module.mk
@@ -1,6 +1,7 @@
 SRC += linux/custom.cpp linux/gdkgl.c linux/gtkglarea.c \
-       linux/dialogs.cpp linux/dlgpiece.cpp linux/gtktools.cpp \
-       linux/main.cpp linux/menu.cpp linux/system.cpp linux/toolbar.cpp
+       linux/dialogs.cpp linux/dlgpiece.cpp linux/dlgfile.cpp \
+       linux/gtktools.cpp linux/main.cpp linux/menu.cpp \
+       linux/system.cpp linux/toolbar.cpp
 
 CFLAGS += $(shell gtk-config --cflags)
 CXXFLAGS += $(shell gtk-config --cflags)

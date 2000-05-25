fd5621cb883baf09be991666b2f38df085a65861
leo <leozide@users.noreply.github.com>
2000-05-25 15:19:53 +0000

    Better texture loading code, Replaced calls to gluBuild2DMipmaps with a custom mipmap function.

--- common/texture.h
+++ common/texture.h
@@ -5,46 +5,45 @@
 #ifndef _TEXTURE_H
 #define _TEXTURE_H
 
-#ifndef GLuint
-#include <GL/gl.h>
-#endif
-
 class File;
 
 typedef enum { LC_INTENSITY, LC_RGB, LC_RGBA } LC_TEXTURE_TYPES;
 
 class Texture
 {
-public:
-	Texture();
-	~Texture();
+ public:
+  Texture();
+  ~Texture();
 
-	void MakeCurrent()
-	{
-      if (m_nID != 0)
-        { glBindTexture(GL_TEXTURE_2D, m_nID); }
-	}
+  void MakeCurrent()
+  {
+    if (m_nID != 0)
+      glBindTexture(GL_TEXTURE_2D, m_nID);
+  }
 
-	bool IsLoaded()
-		{ return glIsTexture(m_nID) == GL_TRUE; }
-	void Load(bool bFilter);
-	bool LoadFromFile(char* strFilename, bool bFilter);
-	void Unload();
+  bool IsLoaded()
+    { return ((m_nID != 0) && (glIsTexture(m_nID) == GL_TRUE)); }
+  void Load(bool bFilter);
+  bool LoadFromFile(char* strFilename, bool bFilter);
+  void Unload();
 
-	void LoadIndex(File* idx);
-	void AddRef(bool bFilter);
-	void DeRef();
+  void LoadIndex(File* idx);
+  void AddRef(bool bFilter);
+  void DeRef();
 
-	// Read-only
-	char m_strName[9];
-	unsigned short m_nWidth;
-	unsigned short m_nHeight;
+  // Read-only
+  char m_strName[9];
+  unsigned short m_nWidth;
+  unsigned short m_nHeight;
 
 protected:
-	int m_nRef;
-	GLuint m_nID;
-	GLenum m_nType;
-	unsigned long m_nOffset;
+  bool FinishLoadImage (bool bFilter, void *data);
+
+  int m_nRef;
+  GLuint m_nID;
+  GLenum m_nFormat;
+  unsigned long m_nOffset;
+  unsigned long m_nFileSize;
 };
 
 

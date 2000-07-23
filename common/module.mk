2da27b2f5d910d0ce3f9fd9c6b648c3608d3b31e
leo <leozide@users.noreply.github.com>
2000-07-23 20:29:56 +0000

    Updated source files list

--- common/module.mk
+++ common/module.mk
@@ -1,6 +1,4 @@
-SRC += common/boundbox.cpp common/camera.cpp common/file.cpp common/globals.cpp common/group.cpp common/image.cpp common/im_bmp.cpp common/im_png.cpp common/library.cpp common/light.cpp common/matrix.cpp common/piece.cpp common/pieceinf.cpp common/project.cpp common/quant.cpp common/terrain.cpp common/texture.cpp common/tr.cpp common/vector.cpp
+SRC += common/camera.cpp common/file.cpp common/globals.cpp common/group.cpp common/image.cpp common/im_bmp.cpp common/im_png.cpp common/library.cpp common/light.cpp common/matrix.cpp common/piece.cpp common/pieceinf.cpp common/project.cpp common/quant.cpp common/terrain.cpp common/texture.cpp common/tr.cpp common/vector.cpp common/opengl.cpp common/object.cpp
 
-#LIBS += -lGLU -lGL -ljpeg -lpng -lm -lX11
-# Work around for debian bug in mesag-dev
-LIBS += -lGLU -lGL -ljpeg -lpng -lz -lm -lX11
+LIBS += -ljpeg -lpng -lz -lm
 

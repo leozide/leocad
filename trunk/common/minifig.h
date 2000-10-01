5d14db0e465be21a082e90a25fc1239003226cc8
leo <leo@c7d43263-9d01-0410-8a33-9dba5d9f93d6>
2000-10-01 14:44:32 +0000

    Save custom minifigs created with the Minifig Wizard

--- trunk/common/minifig.h
+++ trunk/common/minifig.h
@@ -28,6 +28,8 @@ typedef struct
   char name[9];
   char description[32];
   int type;
+  float x, y, z;
+  float rx, ry, rz;
 } LC_MFW_PIECEINFO;
 
 class MinifigWizard
@@ -40,21 +42,28 @@ class MinifigWizard
   void Resize (int width, int height);
   void Calculate ();
   void GetDescriptions (int type, char ***names, int *count);
+  void GetSelections (char **names);
   void ChangePiece (int type, const char *description);
   void ChangeColor (int type, int color);
   void ChangeAngle (int type, float angle);
 
+  void GetMinifigNames (char ***names, int *count);
+  void SaveMinifig (const char* name);
+  bool LoadMinifig (const char* name);
+  void DeleteMinifig (const char* name);
+
  public:
   PieceInfo* m_Info[LC_MFW_NUMITEMS];
   int m_Colors[LC_MFW_NUMITEMS];
+  float m_Angles[LC_MFW_NUMITEMS];
   float	m_Position[LC_MFW_NUMITEMS][3];
   float	m_Rotation[LC_MFW_NUMITEMS][4];
 
  protected:
-  // internal variables used to calculate the real position/rotation
-  float	m_Pos[LC_MFW_NUMITEMS][3];
-  float	m_Rot[LC_MFW_NUMITEMS][3];
-  float m_Angles[LC_MFW_NUMITEMS];
+  // saved minifig templates
+  int  m_MinifigCount;
+  char **m_MinifigNames;
+  char **m_MinifigTemplates;
 };
 
 #endif // _MINIFIG_H_

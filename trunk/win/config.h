a03fa58edbba7983e08b455fa77377a840150b6d
leo <leo@c7d43263-9d01-0410-8a33-9dba5d9f93d6>
2000-12-18 12:46:59 +0000

    Updated version number

--- trunk/win/config.h
+++ trunk/win/config.h
@@ -8,10 +8,10 @@
 #define _CONFIG_H_
 
 #define LC_VERSION_MAJOR 0
-#define LC_VERSION_MINOR 72
+#define LC_VERSION_MINOR 73
 #define LC_VERSION_PATCH 0
 #define LC_VERSION_OSNAME "Windows"
-#define LC_VERSION "0.72.0"
+#define LC_VERSION "0.73.0"
 #define LC_INSTALL_PREFIX "C:\\leocad"
 
 #define LC_LITTLE_ENDIAN

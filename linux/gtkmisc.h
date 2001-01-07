96f824d5fd4dcb4e5c2cf23bbe27104c6d0c6553
leo <leozide@users.noreply.github.com>
2001-01-07 15:17:20 +0000

    Added icons to the menus

--- linux/gtkmisc.h
+++ linux/gtkmisc.h
@@ -11,6 +11,8 @@ GtkWidget* create_menu_in_menu (GtkWidget *menu, gchar *label, GtkAccelGroup *me
 				GtkAccelGroup **submenu_accel);
 GtkWidget* create_menu_item (GtkWidget *menu, gchar *label, GtkAccelGroup *menu_accel,
 			     GtkSignalFunc func, GtkObject *window, int id, const char* data);
+GtkWidget* create_pixmap_menu_item (GtkWidget *menu, gchar *label, gchar **pixmap, GtkAccelGroup *menu_accel,
+                                    GtkSignalFunc func, GtkObject *window, int id, const char* data);
 GtkWidget* create_check_menu_item (GtkWidget *menu, gchar *label, GtkAccelGroup *menu_accel,
 				   GtkSignalFunc func, GtkObject *window, int id, const char* data);
 GtkWidget* create_radio_menu_item (GtkWidget *menu, GtkWidget *last, gchar *label,
--- linux/module.mk
+++ linux/module.mk
@@ -1,8 +1,8 @@
-SRC += linux/profile.cpp linux/gdkgl.c linux/gtkglarea.c \
+SRC += linux/profile.cpp linux/pixmenu.c \
        linux/dialogs.cpp linux/dlgpiece.cpp linux/dlgfile.cpp \
        linux/gtktools.cpp linux/main.cpp linux/menu.cpp \
        linux/system.cpp linux/toolbar.cpp linux/gtkmisc.cpp \
-       linux/linux_gl.cpp
+       linux/linux_gl.cpp linux/basewnd.cpp linux/glwindow.cpp
 
 CFLAGS += $(shell $(GTK_CONFIG) --cflags)
 CXXFLAGS += $(shell $(GTK_CONFIG) --cflags)

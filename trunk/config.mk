8f72ed7db0b515e0101cb6dfc3d14cabfaaa70b3
leo <leo@c7d43263-9d01-0410-8a33-9dba5d9f93d6>
2001-01-23 15:05:35 +0000

    Determine size of variables

--- trunk/config.mk
+++ trunk/config.mk
@@ -78,7 +78,123 @@ config:
 	@echo "#define LC_INSTALL_PREFIX \"$(PREFIX)\"" >> $(OSDIR)/config.h
 	@echo "" >> $(OSDIR)/config.h
 
-### Determine if machine is little or big endian
+### Determine variable sizes
+	@echo -n "checking size of char... "; \
+	echo "#include <stdio.h>" > conftest.c; \
+	echo "main() { FILE *f=fopen(\"conftestval\", \"w\");" >> conftest.c; \
+	echo "if (!f) exit(1); fprintf(f, \"%d\\n\", sizeof(char)); exit(0); }" >> conftest.c; \
+	if { (eval $(CC) conftest.c -o conftest) 2> /dev/null; } && \
+	  (test -s conftest && (./conftest; exit) 2> /dev/null); then \
+	  ac_cv_sizeof_char=`cat conftestval`; \
+	  echo "$$ac_cv_sizeof_char"; \
+	else \
+	  echo "failed to get size of char"; \
+	  ac_cv_sizeof_char=0; \
+	fi; \
+	echo "#define LC_SIZEOF_CHAR $$ac_cv_sizeof_char" >> $(OSDIR)/config.h; \
+	rm -f conftest.c conftest conftestval; \
+	\
+	echo -n "checking size of short... "; \
+	echo "#include <stdio.h>" > conftest.c; \
+	echo "main() { FILE *f=fopen(\"conftestval\", \"w\");" >> conftest.c; \
+	echo "if (!f) exit(1); fprintf(f, \"%d\\n\", sizeof(short)); exit(0); }" >> conftest.c; \
+	if { (eval $(CC) conftest.c -o conftest) 2> /dev/null; } && \
+	  (test -s conftest && (./conftest; exit) 2> /dev/null); then \
+	  ac_cv_sizeof_short=`cat conftestval`; \
+	  echo "$$ac_cv_sizeof_short"; \
+	else \
+	  echo "failed to get size of short"; \
+	  ac_cv_sizeof_short=0; \
+	fi; \
+	echo "#define LC_SIZEOF_SHORT $$ac_cv_sizeof_short" >> $(OSDIR)/config.h; \
+	rm -f conftest.c conftest conftestval; \
+	\
+	echo -n "checking size of long... "; \
+	echo "#include <stdio.h>" > conftest.c; \
+	echo "main() { FILE *f=fopen(\"conftestval\", \"w\");" >> conftest.c; \
+	echo "if (!f) exit(1); fprintf(f, \"%d\\n\", sizeof(long)); exit(0); }" >> conftest.c; \
+	if { (eval $(CC) conftest.c -o conftest) 2> /dev/null; } && \
+	  (test -s conftest && (./conftest; exit) 2> /dev/null); then \
+	  ac_cv_sizeof_long=`cat conftestval`; \
+	  echo "$$ac_cv_sizeof_long"; \
+	else \
+	  echo "failed to get size of long"; \
+	  ac_cv_sizeof_long=0; \
+	fi; \
+	echo "#define LC_SIZEOF_LONG $$ac_cv_sizeof_long" >> $(OSDIR)/config.h; \
+	rm -f conftest.c conftest conftestval; \
+	\
+	echo -n "checking size of int... "; \
+	echo "#include <stdio.h>" > conftest.c; \
+	echo "main() { FILE *f=fopen(\"conftestval\", \"w\");" >> conftest.c; \
+	echo "if (!f) exit(1); fprintf(f, \"%d\\n\", sizeof(int)); exit(0); }" >> conftest.c; \
+	if { (eval $(CC) conftest.c -o conftest) 2> /dev/null; } && \
+	  (test -s conftest && (./conftest; exit) 2> /dev/null); then \
+	  ac_cv_sizeof_int=`cat conftestval`; \
+	  echo "$$ac_cv_sizeof_int"; \
+	else \
+	  echo "failed to get size of int"; \
+	  ac_cv_sizeof_int=0; \
+	fi; \
+	echo "#define LC_SIZEOF_INT $$ac_cv_sizeof_int" >> $(OSDIR)/config.h; \
+	rm -f conftest.c conftest conftestval; \
+	\
+	echo -n "checking size of void *... "; \
+	echo "#include <stdio.h>" > conftest.c; \
+	echo "main() { FILE *f=fopen(\"conftestval\", \"w\");" >> conftest.c; \
+	echo "if (!f) exit(1); fprintf(f, \"%d\\n\", sizeof(void *)); exit(0); }" >> conftest.c; \
+	if { (eval $(CC) conftest.c -o conftest) 2> /dev/null; } && \
+	  (test -s conftest && (./conftest; exit) 2> /dev/null); then \
+	  ac_cv_sizeof_void_p=`cat conftestval`; \
+	  echo "$$ac_cv_sizeof_void_p"; \
+	else \
+	  echo "failed to get size of void *"; \
+	  ac_cv_sizeof_void_p=0; \
+	fi; \
+	echo "#define LC_SIZEOF_VOID_P $$ac_cv_sizeof_void_p" >> $(OSDIR)/config.h; \
+	rm -f conftest.c conftest conftestval; \
+	\
+	echo -n "checking size of long long... "; \
+	echo "#include <stdio.h>" > conftest.c; \
+	echo "main() { FILE *f=fopen(\"conftestval\", \"w\");" >> conftest.c; \
+	echo "if (!f) exit(1); fprintf(f, \"%d\\n\", sizeof(long long)); exit(0); }" >> conftest.c; \
+	if { (eval $(CC) conftest.c -o conftest) 2> /dev/null; } && \
+	  (test -s conftest && (./conftest; exit) 2> /dev/null); then \
+	  ac_cv_sizeof_long_long=`cat conftestval`; \
+	  echo "$$ac_cv_sizeof_long_long"; \
+	else \
+	  echo "failed to get size of long long"; \
+	  ac_cv_sizeof_long_long=0; \
+	fi; \
+	echo "#define LC_SIZEOF_LONG_LONG $$ac_cv_sizeof_long_long" >> $(OSDIR)/config.h; \
+	rm -f conftest.c conftest conftestval; \
+	case 2 in \
+	  $$ac_cv_sizeof_short)		lcint16=short;; \
+	  $$ac_cv_sizeof_int)		lcint16=int;; \
+	esac; \
+	case 4 in \
+	  $$ac_cv_sizeof_short)		lcint32=short;; \
+	  $$ac_cv_sizeof_int)		lcint32=int;; \
+	  $$ac_cv_sizeof_long)		lcint32=long;; \
+	esac; \
+	echo "" >> $(OSDIR)/config.h; \
+	echo "typedef signed char lcint8;" >> $(OSDIR)/config.h; \
+	echo "typedef unsigned char lcuint8;" >> $(OSDIR)/config.h; \
+	if test -n "$$lcint16"; then \
+	  echo "typedef signed $$lcint16 lcint16;" >> $(OSDIR)/config.h; \
+	  echo "typedef unsigned $$lcint16 lcuint16;" >> $(OSDIR)/config.h; \
+	else \
+	  echo "#error need to define lcint16 and lcuint16" >> $(OSDIR)/config.h; \
+	fi; \
+	if test -n "$$lcint32"; then \
+	echo "typedef signed $$lcint32 lcint32;" >> $(OSDIR)/config.h; \
+	echo "typedef unsigned $$lcint32 lcuint32;" >> $(OSDIR)/config.h; \
+	else \
+	  echo "#error need to define lcint32 and lcuint32" >> $(OSDIR)/config.h; \
+	fi; \
+	echo "" >> $(OSDIR)/config.h
+
+### Check if machine is little or big endian
 	@echo -n "Determining endianess... "
 	@echo "main () { union { long l; char c[sizeof (long)]; } u;" > endiantest.c
 	@echo "u.l = 1; exit (u.c[sizeof (long) - 1] == 1); }" >> endiantest.c

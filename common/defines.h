13c9416c5957e67930026b96f02b09abb7cbfda2
leo <leozide@users.noreply.github.com>
2001-12-05 23:58:47 +0000

    Remove obsolete endianess code.

--- common/defines.h
+++ common/defines.h
@@ -51,19 +51,6 @@ char* strlwr(char* string);
 
 #endif
 
-// Endianess handling (files are little endian).
-#ifdef LC_BIG_ENDIAN
-unsigned short SwapSHORT(unsigned short x)
-{ return (x>>8) | (x<<8); }
-unsigned long SwapLONG(unsigned long x)
-{ return (x>>24) | ((x>>8) & 0xff00) | ((x<<8) & 0xff0000) | (x<<24); }
-#define SHORT(x) ((short)SwapSHORT((unsigned short) (x)))
-#define LONG(x)  ((long)SwapLONG((unsigned long) (x)))
-#else
-#define SHORT(x) (x)
-#define LONG(x)  (x)
-#endif
-
 /////////////////////////////////////////////////////////////////////////////
 // LeoCAD constants
 

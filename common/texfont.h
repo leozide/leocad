6bc035c98ae1a28cbdbe5ad76f4d42e314e6e6c3
leo <leozide@users.noreply.github.com>
2005-02-11 19:06:36 +0000

    Added GetStringDimensions() function.

--- common/texfont.h
+++ common/texfont.h
@@ -9,28 +9,30 @@ class Texture;
 class TexFont
 {
 public:
-  TexFont ();
-  ~TexFont ();
+	TexFont ();
+	~TexFont ();
 
-  bool IsLoaded () const
-    { return m_bLoaded; }
-  void MakeCurrent ()
-    { if (m_bLoaded) m_pTexture->MakeCurrent (); }
+	bool IsLoaded () const
+	{ return m_bLoaded; }
+	void MakeCurrent ()
+	{ if (m_bLoaded) m_pTexture->MakeCurrent (); }
 
-  bool FileLoad (File& file);
-  void PrintText (float left, float top, const char* text) const;
-  void PrintCharScaled (float scale, char ch) const;
+	bool FileLoad(File& file);
+	void PrintText(float left, float top, const char* text) const;
+	void PrintText(float Left, float Top, float ScaleX, float ScaleY, const char* Text) const;
+	void PrintCharScaled(float scale, char ch) const;
+	void GetStringDimensions(int* cx, int* cy, const char* Text) const;
 
 protected:
-  struct 
-  {
-    unsigned char width;
-    float left, right, top, bottom;
-  } m_Glyphs[256];
+	struct
+	{
+		unsigned char width;
+		float left, right, top, bottom;
+	} m_Glyphs[256];
 
-  Texture* m_pTexture;
-  unsigned char m_nFontHeight;
-  bool m_bLoaded;
+	Texture* m_pTexture;
+	unsigned char m_nFontHeight;
+	bool m_bLoaded;
 };
 
 #endif // _TEXFONT_H_

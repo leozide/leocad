e89462b0664a04f7752ef5e20dadb08f2fd09592
leo <leozide@users.noreply.github.com>
2006-09-02 06:02:47 +0000

    Updated project and menu.

--- win/resource.h
+++ win/resource.h
@@ -680,6 +680,8 @@
 #define ID_LIBDLG_CATEGORY_RESET        33166
 #define ID_SNAP_XY                      33167
 #define ID_SNAP_Z                       33168
+#define ID_VIEW_SPLITHORIZONTALLY       33169
+#define ID_VIEW_SPLITVERTICALLY         33170
 #define ID_VIEW_PIECES_BAR              59425
 #define ID_VIEW_TOOLS_BAR               59426
 #define ID_VIEW_ANIMATION_BAR           59427
@@ -691,7 +693,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_3D_CONTROLS                     1
 #define _APS_NEXT_RESOURCE_VALUE        240
-#define _APS_NEXT_COMMAND_VALUE         33169
+#define _APS_NEXT_COMMAND_VALUE         33172
 #define _APS_NEXT_CONTROL_VALUE         1247
 #define _APS_NEXT_SYMED_VALUE           121
 #endif

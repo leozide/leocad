3a91fa28aaee6e3da7de2025496ed11160bc4dfa
leo <leozide@users.noreply.github.com>
2006-09-10 22:47:16 +0000

    Changed u64 type.

--- win/config.h
+++ win/config.h
@@ -21,8 +21,8 @@ typedef signed short i16;
 typedef unsigned short u16;
 typedef signed int i32;
 typedef unsigned int u32;
-typedef long long i64;
-typedef unsigned long long u64;
+typedef __int64 i64;
+typedef unsigned __int64 u64;
 
 #define LC_LITTLE_ENDIAN
 #define LCUINT16(val) val
@@ -31,6 +31,7 @@ typedef unsigned long long u64;
 #define LCINT32(val) val
 #define LCFLOAT(val) val
 
+#undef LC_HAVE_3DSFTK
 #undef LC_HAVE_JPEGLIB
 #undef LC_HAVE_ZLIB
 #undef LC_HAVE_PNGLIB

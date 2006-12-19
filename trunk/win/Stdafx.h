d4a2a38507ad07e2cc654e9a25b83c152828b8c2
leo <leo@c7d43263-9d01-0410-8a33-9dba5d9f93d6>
2006-12-19 16:34:43 +0000

    Fixes for VC6.

--- trunk/win/Stdafx.h
+++ trunk/win/Stdafx.h
@@ -15,6 +15,8 @@
 #define VC_EXTRALEAN          // Exclude rarely-used stuff from Windows headers
 #endif
 
+#if _MSC_VER >= 1400
+
 #ifndef WINVER                // Allow use of features specific to Windows XP or later.
 #define WINVER 0x0501         // Change this to the appropriate value to target other versions of Windows.
 #endif
@@ -29,6 +31,8 @@
 
 #ifndef _WIN32_IE             // Allow use of features specific to IE 6.0 or later.
 #define _WIN32_IE 0x0600      // Change this to the appropriate value to target other versions of IE.
+#endif
+
 #endif
 
 // turns off MFC's hiding of some common and often safely ignored warning messages
@@ -51,7 +55,7 @@ typedef HTASK ACTIVATEAPPPARAM;
 #if _MFC_VER < 0x0800
 #define _strcmpi strcmp
 #define _strlwr strlwr
-#define _strlwr strupr
+#define _strupr strupr
 #endif
 
 #if _MFC_VER >= 0x0800

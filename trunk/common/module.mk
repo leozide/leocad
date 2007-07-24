82f18e605055efd1bdfc3f0353d784c9e8f8cb2a
leo <leo@c7d43263-9d01-0410-8a33-9dba5d9f93d6>
2007-07-24 22:29:02 +0000

    Linux fixes.

--- trunk/common/module.mk
+++ trunk/common/module.mk
@@ -2,7 +2,7 @@ SRC += common/algebra.cpp common/lc_camera.cpp common/lc_camera_target.cpp commo
        common/lc_flexpiece.cpp common/globals.cpp common/group.cpp common/image.cpp common/im_bmp.cpp common/im_gif.cpp \
        common/lc_application.cpp common/lc_mesh.cpp common/library.cpp common/lc_light.cpp common/mainwnd.cpp \
        common/matrix.cpp common/message.cpp common/minifig.cpp common/lc_model.cpp common/lc_modelref.cpp common/lc_object.cpp common/opengl.cpp \
-       common/lc_piece.cpp common/lc_pieceobj.cpp common/pieceinf.cpp common/preview.cpp common/project.cpp common/quant.cpp common/lc_scene.cpp \
+       common/lc_piece.cpp common/lc_pieceobj.cpp common/pieceinf.cpp common/lc_pivot.cpp common/preview.cpp common/project.cpp common/quant.cpp common/lc_scene.cpp \
        common/str.cpp common/terrain.cpp common/texfont.cpp common/texture.cpp common/tr.cpp \
        common/view.cpp
 

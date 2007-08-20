f5692fa2fd5bd04f770d103cdb246fdd36daa9d6
leo <leo@c7d43263-9d01-0410-8a33-9dba5d9f93d6>
2007-08-20 18:02:40 +0000

    Fixed minifig default colors.

--- trunk/common/minifig.cpp
+++ trunk/common/minifig.cpp
@@ -15,6 +15,7 @@
 #include "matrix.h"
 #include "library.h"
 #include "lc_application.h"
+#include "lc_colors.h"
 
 // =============================================================================
 // Static variables
@@ -434,14 +435,22 @@ static int mfw_pieces = sizeof(mfw_pieceinfo)/sizeof(LC_MFW_PIECEINFO);
 MinifigWizard::MinifigWizard(GLWindow *share)
 	: GLWindow(share)
 {
-	const unsigned char colors[LC_MFW_NUMITEMS] = { 0, 6, 4, 22, 0, 0, 6, 6, 22, 22, 9, 9, 9, 22, 22 };
+	const int colors[LC_MFW_NUMITEMS] = { 4, 14, 1, 6, 4, 4, 14, 14, 6, 6, 0, 0, 0, 6, 6 };
 	const char *pieces[LC_MFW_NUMITEMS] = { "3624", "3626BP01", "973", "None", "976", "975", "977", "977",
 	                                        "None", "None", "970", "972", "971", "None", "None" };
 	int i;
 
 	for (i = 0; i < LC_MFW_NUMITEMS; i++)
 	{
-		m_Colors[i] = colors[i];
+		m_Colors[i] = 0;
+
+		for (int j = 0; j < lcNumUserColors; j++)
+			if (lcColorList[j].Code == colors[i])
+			{
+				m_Colors[i] = j;
+				break;
+			}
+
 		m_Angles[i] = 0;
 
 		m_Info[i] = lcGetPiecesLibrary()->FindPieceInfo(pieces[i]);

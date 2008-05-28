42d21be22d166de94dbe8b40ba4afe519cfae75c
leo <leozide@users.noreply.github.com>
2008-05-28 18:29:57 +0000

    Linux fixes.

--- linux/gtktools.h
+++ linux/gtktools.h
@@ -2,7 +2,7 @@
 #ifndef _GTKTOOLS_H_
 #define _GTKTOOLS_H_
 
-GtkWidget* new_pixmap (GtkWidget *widget, char **data);
+GtkWidget* new_pixmap (GtkWidget *widget, const char **data);
 GtkWidget* clist_title_with_arrow (GtkWidget* clist, char col, char* label_text);
 void set_notebook_tab (GtkWidget *notebook, gint page_num, GtkWidget *widget);
 void set_button_pixmap (GtkWidget* widget, float* color);

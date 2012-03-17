c1a3e8034b0e6aba716c3b1b2d9132c161f663de
leo <leo@c7d43263-9d01-0410-8a33-9dba5d9f93d6>
2012-03-17 01:51:06 +0000

    Updated Linux dialogs.

--- trunk/linux/dialogs.h
+++ trunk/linux/dialogs.h
@@ -1,11 +1,9 @@
 #ifndef _DIALOGS_H_
 #define _DIALOGS_H_
 
-// Dialog support functions
+// TODO: obsolete, delete
 void dialog_button_callback(GtkWidget *widget, gpointer data);
 gint dialog_delete_callback(GtkWidget *widget, GdkEvent* event, gpointer data);
-
-// obsolete
 int dlg_domodal(GtkWidget* dlg, int def);
 void dlg_end(int ret);
 gint dlg_delete_callback(GtkWidget *widget, GdkEvent* event, gpointer data);
@@ -18,12 +16,12 @@ int openprojectdlg_execute(char* filename);
 int saveprojectdlg_execute(char* filename);
 int savepicturedlg_execute(void* param);
 int msgbox_execute(const char* text, const char *caption, int flags);
-int filedlg_execute(const char* caption, char* filename);
 int arraydlg_execute(void* param);
 int aboutdlg_execute(void* param);
 int htmldlg_execute(void* param);
 int imageoptsdlg_execute(GtkWidget* parent, void* param, bool from_htmldlg);
 int povraydlg_execute(void* param);
+int wavefrontdlg_execute(void* param);
 int preferencesdlg_execute(void* param);
 int propertiesdlg_execute(void* param);
 int groupeditdlg_execute(void* param);

d6090633b10b798338e7b6ffd69ff96d65ba0ecd
leo <leo@c7d43263-9d01-0410-8a33-9dba5d9f93d6>
2012-04-13 02:14:48 +0000

    OpenGL cleanup.

--- trunk/win/win_gl.cpp
+++ trunk/win/win_gl.cpp
@@ -1,41 +1,6 @@
 #include "lc_global.h"
-#include "opengl.h"
-
-BOOL OpenGLSwapBuffers(HDC hdc)
-{
-	return SwapBuffers(hdc);
-}
-
-int OpenGLChoosePixelFormat(HDC hdc, CONST PIXELFORMATDESCRIPTOR * ppfd)
-{
-	return ChoosePixelFormat(hdc, ppfd);
-}
-
-int OpenGLDescribePixelFormat(HDC hdc, int iPixelFormat, UINT nBytes, LPPIXELFORMATDESCRIPTOR ppfd)
-{
-	return DescribePixelFormat(hdc, iPixelFormat, nBytes, ppfd);
-}
-
-BOOL OpenGLSetPixelFormat(HDC hdc, int iPixelFormat, CONST PIXELFORMATDESCRIPTOR * ppfd)
-{
-	return SetPixelFormat(hdc, iPixelFormat, ppfd);
-}
-
-int OpenGLGetPixelFormat(HDC hdc)
-{
-	return GetPixelFormat(hdc);
-}
 
 void* Sys_GLGetExtension(const char* Symbol)
 {
 	return wglGetProcAddress(Symbol);
-}
-
-bool Sys_GLOpenLibrary(const char* libname)
-{
-	return true;
-}
-
-void Sys_GLCloseLibrary()
-{
 }

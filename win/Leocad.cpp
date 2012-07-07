4369308e5a6d3486704f9e2b85419d5472d18296
leo <leozide@users.noreply.github.com>
2012-07-07 01:41:09 +0000

    Fixed tooltips showing under toolbars.

--- win/Leocad.cpp
+++ win/Leocad.cpp
@@ -155,6 +155,16 @@ CCADApp theApp;
 
 BOOL CCADApp::InitInstance()
 {
+	// InitCommonControlsEx() is required on Windows XP if an application
+	// manifest specifies use of ComCtl32.dll version 6 or later to enable
+	// visual styles.  Otherwise, any window creation will fail.
+	INITCOMMONCONTROLSEX InitCtrls;
+	InitCtrls.dwSize = sizeof(InitCtrls);
+	// Set this to include all the common control classes you want to use
+	// in your application.
+	InitCtrls.dwICC = ICC_WIN95_CLASSES;
+	InitCommonControlsEx(&InitCtrls);
+
 	CWinAppEx::InitInstance();
 
 	SetRegistryKey(_T("BT Software"));
--- win/Stdafx.h
+++ win/Stdafx.h
@@ -60,3 +60,11 @@
 //#define PIECEBAR_ZOOMPREVIEW  0x08 // Zoom piece preview
 //#define PIECEBAR_COMBO        0x10 // Show combobox
 //#define PIECEBAR_PARTNUMBERS  0x20 // Show part numbers
+
+#if defined _M_IX86
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#elif defined _M_X64
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='amd64' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#else
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#endif

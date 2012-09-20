21f5da2f4f4fcd01cc7a744f5e5e4fc8c4bf9719
leo <leo@c7d43263-9d01-0410-8a33-9dba5d9f93d6>
2012-09-20 00:53:12 +0000

    Added unzip support.

---
+++ trunk/common/lc_library.cpp
@@ -0,0 +1,15 @@
+#include "lc_global.h"
+#include "lc_library.h"
+
+lcPiecesLibrary::lcPiecesLibrary()
+{
+}
+
+lcPiecesLibrary::~lcPiecesLibrary()
+{
+}
+
+bool lcPiecesLibrary::OpenArchive(const char* LibPath)
+{
+	return true;
+}

e58cf7dc36c9913620c07836c9f6abceda8b9d3f
leo <leo@c7d43263-9d01-0410-8a33-9dba5d9f93d6>
2013-01-21 23:32:18 +0000

    LeoCAD 0.79.2 tag.

---
+++ tags/leocad-0.79.2/common/lc_zipfile.h
@@ -0,0 +1,81 @@
+#ifndef _LC_ZIPFILE_H_
+#define _LC_ZIPFILE_H_
+
+#include "array.h"
+
+class lcFile;
+
+// Date/time info.
+struct tm_unz
+{
+	lcuint32 tm_sec;            // seconds after the minute - [0,59]
+	lcuint32 tm_min;            // minutes after the hour - [0,59]
+	lcuint32 tm_hour;           // hours since midnight - [0,23]
+	lcuint32 tm_mday;           // day of the month - [1,31]
+	lcuint32 tm_mon;            // months since January - [0,11]
+	lcuint32 tm_year;           // years - [1980..2044]
+};
+
+// Information about a file in the zipfile.
+struct lcZipFileInfo
+{
+	lcuint16 version;              // version made by                 2 bytes
+	lcuint16 version_needed;       // version needed to extract       2 bytes
+	lcuint16 flag;                 // general purpose bit flag        2 bytes
+	lcuint16 compression_method;   // compression method              2 bytes
+	lcuint32 dosDate;              // last mod file date in Dos fmt   4 bytes
+	lcuint32 crc;                  // crc-32                          4 bytes
+	lcuint64 compressed_size;      // compressed size                 8 bytes
+	lcuint64 uncompressed_size;    // uncompressed size               8 bytes
+	lcuint16 size_filename;        // filename length                 2 bytes
+	lcuint16 size_file_extra;      // extra field length              2 bytes
+	lcuint16 size_file_comment;    // file comment length             2 bytes
+
+	lcuint16 disk_num_start;       // disk number start               2 bytes
+	lcuint16 internal_fa;          // internal file attributes        2 bytes
+	lcuint32 external_fa;          // external file attributes        4 bytes
+
+	lcuint64 offset_curfile;       // relative offset of local header 8 bytes
+	char file_name[256];
+	tm_unz tmu_date;
+
+	lcMemFile* write_buffer;
+	bool deleted;
+};
+
+class lcZipFile
+{
+public:
+	lcZipFile();
+	~lcZipFile();
+
+	bool OpenRead(const char* FilePath);
+	bool OpenWrite(const char* FilePath, bool Append);
+
+	bool ExtractFile(int FileIndex, lcMemFile& File, lcuint32 MaxLength = 0xffffffff);
+	bool ExtractFile(const char* FileName, lcMemFile& File, lcuint32 MaxLength = 0xffffffff);
+	bool AddFile(const char* FileName, lcMemFile& File);
+	bool DeleteFile(const char* FileName);
+
+	ObjArray<lcZipFileInfo> mFiles;
+
+protected:
+	bool Open();
+	void Flush();
+	bool ReadCentralDir();
+	lcuint64 SearchCentralDir();
+	lcuint64 SearchCentralDir64();
+	bool CheckFileCoherencyHeader(int FileIndex, lcuint32* SizeVar, lcuint64* OffsetLocalExtraField, lcuint32* SizeLocalExtraField);
+
+	lcDiskFile* mFile;
+
+	bool mModified;
+	bool mZip64;
+	lcuint64 mNumEntries;
+	lcuint64 mCentralDirSize;
+	lcuint64 mCentralDirOffset;
+	lcuint64 mBytesBeforeZipFile;
+	lcuint64 mCentralPos;
+};
+
+#endif // _LC_ZIPFILE_H_
---
+++ tags/leocad-0.79.2/win/Htmldlg.cpp
@@ -0,0 +1,100 @@
+#include "lc_global.h"
+#include "leocad.h"
+#include "HTMLDlg.h"
+#include "ImageDlg.h"
+#include "Tools.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+/////////////////////////////////////////////////////////////////////////////
+// CHTMLDlg dialog
+
+
+CHTMLDlg::CHTMLDlg(void* param, CWnd* pParent /*=NULL*/)
+	: CDialog(CHTMLDlg::IDD, pParent)
+{
+	imopts = param;
+
+	//{{AFX_DATA_INIT(CHTMLDlg)
+	m_nLayout = 0;
+	m_bIndex = FALSE;
+	m_strFolder = _T("");
+	m_bImages = FALSE;
+	m_bListEnd = FALSE;
+	m_bListStep = FALSE;
+	m_bHighlight = FALSE;
+	//}}AFX_DATA_INIT
+}
+
+
+void CHTMLDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+	//{{AFX_DATA_MAP(CHTMLDlg)
+	DDX_Radio(pDX, IDC_HTMDLG_SINGLEPAGE, m_nLayout);
+	DDX_Check(pDX, IDC_HTMDLG_INDEX, m_bIndex);
+	DDX_Text(pDX, IDC_HTMDLG_OUTPUT, m_strFolder);
+	DDX_Check(pDX, IDC_HTMDLG_LISTIMAGES, m_bImages);
+	DDX_Check(pDX, IDC_HTMDLG_LIST_END, m_bListEnd);
+	DDX_Check(pDX, IDC_HTMDLG_LIST_STEP, m_bListStep);
+	DDX_Check(pDX, IDC_HTMDLG_HIGHLIGHT, m_bHighlight);
+	//}}AFX_DATA_MAP
+}
+
+
+BEGIN_MESSAGE_MAP(CHTMLDlg, CDialog)
+	//{{AFX_MSG_MAP(CHTMLDlg)
+	ON_BN_CLICKED(IDC_HTMDLG_IMAGEOPTIONS, OnImageOptions)
+	ON_BN_CLICKED(IDC_HTMDLG_LIST_STEP, OnListClick)
+	ON_BN_CLICKED(IDC_HTMDLG_SINGLEPAGE, OnLayoutClick)
+	ON_BN_CLICKED(IDC_HTMDLG_BROWSEFOLDER, OnHtmdlgBrowsefolder)
+	ON_BN_CLICKED(IDC_HTMDLG_LIST_END, OnListClick)
+	ON_BN_CLICKED(IDC_HTMDLG_ONESTEP, OnLayoutClick)
+	//}}AFX_MSG_MAP
+END_MESSAGE_MAP()
+
+/////////////////////////////////////////////////////////////////////////////
+// CHTMLDlg message handlers
+
+void CHTMLDlg::OnImageOptions() 
+{
+	CImageDlg dlg(TRUE, imopts);
+	dlg.DoModal();
+}
+
+void CHTMLDlg::OnListClick()
+{
+	UpdateData();
+	GetDlgItem(IDC_HTMDLG_LISTIMAGES)->EnableWindow(m_bListStep || m_bListEnd);
+}
+
+void CHTMLDlg::OnLayoutClick() 
+{
+	UpdateData();
+	GetDlgItem(IDC_HTMDLG_INDEX)->EnableWindow(m_nLayout != 0);
+}
+
+void CHTMLDlg::OnHtmdlgBrowsefolder() 
+{
+	CString str;
+	if (FolderBrowse(&str, _T("Select Output Folder."), GetSafeHwnd()))
+	{
+		UpdateData (TRUE);
+		m_strFolder = str;
+		UpdateData (FALSE);
+	}
+}
+
+BOOL CHTMLDlg::OnInitDialog() 
+{
+	CDialog::OnInitDialog();
+	
+	GetDlgItem(IDC_HTMDLG_LISTIMAGES)->EnableWindow(m_bListStep || m_bListEnd);
+	GetDlgItem(IDC_HTMDLG_INDEX)->EnableWindow(m_nLayout != 0);
+
+	return TRUE;
+}

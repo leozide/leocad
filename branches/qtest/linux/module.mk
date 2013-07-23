b320169fdf6b798090574a0b0f5771d0234f8975
leo <leo@c7d43263-9d01-0410-8a33-9dba5d9f93d6>
2013-07-23 22:19:09 +0000

    Deleted GTK folder.

--- branches/qtest/linux/module.mk
+++
@@ -1,5 +0,0 @@
-SRC += linux/profile.cpp \
-       linux/dialogs.cpp linux/dlgpiece.cpp linux/dlgfile.cpp \
-       linux/gtktools.cpp linux/main.cpp linux/menu.cpp \
-       linux/system.cpp linux/toolbar.cpp linux/gtkmisc.cpp \
-       linux/linux_gl.cpp linux/basewnd.cpp linux/glwindow.cpp linux/libdlg.cpp

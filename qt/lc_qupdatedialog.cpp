3450d40fb63f77aa647994fd68fc76c127008731
leo <leozide@users.noreply.github.com>
2013-08-12 00:26:52 +0000

    Added new version check on startup.

--- qt/lc_qupdatedialog.cpp
+++ qt/lc_qupdatedialog.cpp
@@ -3,28 +3,44 @@
 #include "ui_lc_qupdatedialog.h"
 #include "lc_application.h"
 #include "lc_library.h"
+#include "lc_profile.h"
+
+void lcDoInitialUpdateCheck()
+{
+	int updateFrequency = lcGetProfileInt(LC_PROFILE_CHECK_UPDATES);
+
+	if (updateFrequency == 0)
+		return;
+
+	QSettings settings;
+	QDateTime checkTime = settings.value("LastUpdateCheck", QDateTime()).toDateTime();
+
+	if (!checkTime.isNull())
+	{
+		checkTime.addDays(1); // TODO: add option for weekly checks
+
+		if (checkTime > QDateTime::currentDateTimeUtc())
+			return;
+	}
+
+	new lcQUpdateDialog(NULL, (void*)1);
+}
 
 lcQUpdateDialog::lcQUpdateDialog(QWidget *parent, void *data) :
-    QDialog(parent),
-    ui(new Ui::lcQUpdateDialog)
+	QDialog(parent),
+	ui(new Ui::lcQUpdateDialog)
 {
 	ui->setupUi(this);
 
-	if (!data)
-	{
-		ui->status->setText(tr("Connecting to update server..."));
+	initialUpdate = (bool)data;
+	connect(this, SIGNAL(finished(int)), this, SLOT(finished(int)));
 
-		manager = new QNetworkAccessManager(this);
-		connect(manager, SIGNAL(finished(QNetworkReply*)), this, SLOT(replyFinished(QNetworkReply*)));
+	ui->status->setText(tr("Connecting to update server..."));
 
-		updateReply = manager->get(QNetworkRequest(QUrl("http://www.leocad.org/updates.txt")));
-	}
-	else
-	{
-		manager = NULL;
-		updateReply = NULL;
-		parseUpdate((const char*)data);
-	}
+	manager = new QNetworkAccessManager(this);
+	connect(manager, SIGNAL(finished(QNetworkReply*)), this, SLOT(replyFinished(QNetworkReply*)));
+
+	updateReply = manager->get(QNetworkRequest(QUrl("http://www.leocad.org/updates.txt")));
 }
 
 lcQUpdateDialog::~lcQUpdateDialog()
@@ -53,71 +69,87 @@ void lcQUpdateDialog::cancel()
 	QDialog::accept();
 }
 
+void lcQUpdateDialog::finished(int result)
+{
+	if (initialUpdate)
+		deleteLater();
+}
+
 void lcQUpdateDialog::replyFinished(QNetworkReply *reply)
 {
+	bool updateAvailable = false;
+
 	if (reply->error() == QNetworkReply::NoError)
 	{
-		parseUpdate(reply->readAll());
+		int majorVersion, minorVersion, patchVersion;
+		int parts;
+
+		QByteArray replyBytes = reply->readAll();
+		const char *update = replyBytes;
+
+		if (sscanf(update, "%d.%d.%d %d", &majorVersion, &minorVersion, &patchVersion, &parts) == 4)
+		{
+			QString status;
+
+			if (majorVersion > LC_VERSION_MAJOR)
+				updateAvailable = true;
+			else if (majorVersion == LC_VERSION_MAJOR)
+			{
+				if (minorVersion > LC_VERSION_MINOR)
+					updateAvailable = true;
+				else if (minorVersion == LC_VERSION_MINOR)
+				{
+					if (patchVersion > LC_VERSION_PATCH)
+						updateAvailable = true;
+				}
+			}
+
+			if (updateAvailable)
+				status = QString(tr("<p>There's a newer version of LeoCAD available for download (%1.%2.%3).</p>")).arg(majorVersion, minorVersion, patchVersion);
+			else
+				status = tr("<p>You are using the latest LeoCAD version.</p>");
+
+			lcPiecesLibrary* library = lcGetPiecesLibrary();
+
+			if (library->mNumOfficialPieces)
+			{
+				if (parts > library->mNumOfficialPieces)
+				{
+					status += tr("<p>There are new parts available.</p>");
+					updateAvailable = true;
+				}
+				else
+					status += tr("<p>There are no new parts available at this time.</p>");
+			}
+
+			if (updateAvailable)
+			{
+				status += tr("<p>Visit <a href=\"http://www.leocad.org/files/\">http://www.leocad.org/files/</a> to download.</p>");
+			}
+
+			ui->status->setText(status);
+		}
+		else
+			ui->status->setText(tr("Error parsing update information."));
+
+		// TODO: option to ignore a version
+
+		QSettings settings;
+		settings.setValue("LastUpdateCheck", QDateTime::currentDateTimeUtc());
+
+		updateReply = NULL;
 		reply->deleteLater();
 	}
 	else
-	{
 		ui->status->setText(tr("Error connecting to the update server."));
-	}
 
-	updateReply = NULL;
-}
-
-void lcQUpdateDialog::parseUpdate(const char *update)
-{
-	int majorVersion, minorVersion, patchVersion;
-	int parts;
-
-	if (sscanf(update, "%d.%d.%d %d", &majorVersion, &minorVersion, &patchVersion, &parts) == 4)
+	if (initialUpdate)
 	{
-		QString status;
-		bool updateAvailable = false;
-
-		if (majorVersion > LC_VERSION_MAJOR)
-			updateAvailable = true;
-		else if (majorVersion == LC_VERSION_MAJOR)
-		{
-			if (minorVersion > LC_VERSION_MINOR)
-				updateAvailable = true;
-			else if (minorVersion == LC_VERSION_MINOR)
-			{
-				if (patchVersion > LC_VERSION_PATCH)
-					updateAvailable = true;
-			}
-		}
-
 		if (updateAvailable)
-			status = QString(tr("<p>There's a newer version of LeoCAD available for download (%1.%2.%3).</p>")).arg(majorVersion, minorVersion, patchVersion);
+			show();
 		else
-			status = tr("<p>You are using the latest LeoCAD version.</p>");
-
-		lcPiecesLibrary* library = lcGetPiecesLibrary();
-
-		if (library->mNumOfficialPieces)
-		{
-			if (parts > library->mNumOfficialPieces)
-			{
-				status += tr("<p>There are new parts available.</p>");
-				updateAvailable = true;
-			}
-			else
-				status += tr("<p>There are no new parts available at this time.</p>");
-		}
-
-		if (updateAvailable)
-		{
-			status += tr("<p>Visit <a href=\"http://www.leocad.org/files/\">http://www.leocad.org/files/</a> to download.</p>");
-		}
-
-		ui->status->setText(status);
+			deleteLater();
 	}
-	else
-		ui->status->setText(tr("Error parsing update information."));
 
 	ui->buttonBox->setStandardButtons(QDialogButtonBox::Close);
 }
--- qt/qtmain.cpp
+++ qt/qtmain.cpp
@@ -1,6 +1,7 @@
 #include "lc_global.h"
 #include "lc_application.h"
 #include "lc_qmainwindow.h"
+#include "lc_qupdatedialog.h"
 #include "mainwnd.h"
 #include <QApplication>
 
@@ -9,7 +10,7 @@ int main(int argc, char *argv[])
 	QApplication app(argc, argv);
 
 	QCoreApplication::setOrganizationDomain("leocad.org");
-	QCoreApplication::setOrganizationName("LeoCAD");
+	QCoreApplication::setOrganizationName("LeoCAD Software");
 	QCoreApplication::setApplicationName("LeoCAD");
 	QCoreApplication::setApplicationVersion(LC_VERSION_TEXT);
 
@@ -50,6 +51,8 @@ int main(int argc, char *argv[])
 	lcGetActiveProject()->UpdateInterface();
 	gMainWindow->UpdateRecentFiles();
 	w.show();
+
+	lcDoInitialUpdateCheck();
 
 	int execReturn = app.exec();
 

8477b38020f61fdd34769dc8489e0bff3ae120ff
leo <leozide@users.noreply.github.com>
2013-09-02 18:29:21 +0000

    Updated version number.

--- common/lc_global.h
+++ common/lc_global.h
@@ -6,8 +6,8 @@
 // Version number.
 #define LC_VERSION_MAJOR 0
 #define LC_VERSION_MINOR 80
-#define LC_VERSION_PATCH 0
-#define LC_VERSION_TEXT "0.80.0"
+#define LC_VERSION_PATCH 1
+#define LC_VERSION_TEXT "0.80.1"
 
 // Forward declarations.
 class lcVector2;

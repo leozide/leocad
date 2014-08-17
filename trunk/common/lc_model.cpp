696ffc39fe30f5c15b9b4fc2827a02ecd16c0084
leo <leo@c7d43263-9d01-0410-8a33-9dba5d9f93d6>
2014-08-17 20:44:12 +0000

    Moved selection functions to model.

--- trunk/common/lc_model.cpp
+++ trunk/common/lc_model.cpp
@@ -56,3 +56,235 @@ lcStep lcModel::GetLastStep() const
 
 	return Step;
 }
+
+lcObject* lcModel::GetFocusObject() const
+{
+	for (int PieceIdx = 0; PieceIdx < mPieces.GetSize(); PieceIdx++)
+	{
+		lcPiece* Piece = mPieces[PieceIdx];
+
+		if (Piece->IsFocused())
+			return Piece;
+	}
+
+	for (int CameraIdx = 0; CameraIdx < mCameras.GetSize(); CameraIdx++)
+	{
+		lcCamera* Camera = mCameras[CameraIdx];
+
+		if (Camera->IsFocused())
+			return Camera;
+	}
+
+	for (int LightIdx = 0; LightIdx < mLights.GetSize(); LightIdx++)
+	{
+		lcLight* Light = mLights[LightIdx];
+
+		if (Light->IsFocused())
+			return Light;
+	}
+
+	return NULL;
+}
+
+void lcModel::UpdateSelection() const
+{
+	int Flags = 0;
+	int SelectedCount = 0;
+	lcObject* Focus = NULL;
+
+	if (mPieces.IsEmpty())
+		Flags |= LC_SEL_NO_PIECES;
+	else
+	{
+		lcGroup* pGroup = NULL;
+		bool first = true;
+
+		for (int PieceIdx = 0; PieceIdx < mPieces.GetSize(); PieceIdx++)
+		{
+			lcPiece* Piece = mPieces[PieceIdx];
+
+			if (Piece->IsSelected())
+			{
+				SelectedCount++;
+
+				if (Piece->IsFocused())
+					Focus = Piece;
+
+				Flags |= LC_SEL_PIECE | LC_SEL_SELECTED;
+
+				if (Piece->GetGroup() != NULL)
+				{
+					Flags |= LC_SEL_GROUPED;
+					if (Piece->IsFocused())
+						Flags |= LC_SEL_FOCUS_GROUPED;
+				}
+
+				if (first)
+				{
+					pGroup = Piece->GetGroup();
+					first = false;
+				}
+				else
+				{
+					if (pGroup != Piece->GetGroup())
+						Flags |= LC_SEL_CAN_GROUP;
+					else
+						if (pGroup == NULL)
+							Flags |= LC_SEL_CAN_GROUP;
+				}
+			}
+			else
+			{
+				Flags |= LC_SEL_UNSELECTED;
+
+				if (Piece->IsHidden())
+					Flags |= LC_SEL_HIDDEN;
+			}
+		}
+	}
+
+	for (int CameraIdx = 0; CameraIdx < mCameras.GetSize(); CameraIdx++)
+	{
+		lcCamera* Camera = mCameras[CameraIdx];
+
+		if (Camera->IsSelected())
+		{
+			Flags |= LC_SEL_SELECTED;
+			SelectedCount++;
+
+			if (Camera->IsFocused())
+				Focus = Camera;
+		}
+	}
+
+	for (int LightIdx = 0; LightIdx < mLights.GetSize(); LightIdx++)
+	{
+		lcLight* Light = mLights[LightIdx];
+
+		if (Light->IsSelected())
+		{
+			Flags |= LC_SEL_SELECTED;
+			SelectedCount++;
+
+			if (Light->IsFocused())
+				Focus = Light;
+		}
+	}
+
+	gMainWindow->UpdateSelectedObjects(Flags, SelectedCount, Focus);
+}
+
+void lcModel::ClearSelection(bool UpdateInterface)
+{
+	for (int PieceIdx = 0; PieceIdx < mPieces.GetSize(); PieceIdx++)
+		mPieces[PieceIdx]->SetSelected(false);
+
+	for (int CameraIdx = 0; CameraIdx < mCameras.GetSize(); CameraIdx++)
+		mCameras[CameraIdx]->SetSelected(false);
+
+	for (int LightIdx = 0; LightIdx < mLights.GetSize(); LightIdx++)
+		mLights[LightIdx]->SetSelected(false);
+
+	if (UpdateInterface)
+	{
+		UpdateSelection();
+		gMainWindow->UpdateAllViews();
+		gMainWindow->UpdateFocusObject(NULL);
+	}
+}
+
+void lcModel::SelectGroup(lcGroup* TopGroup, bool Select)
+{
+	if (!TopGroup)
+		return;
+
+	for (int PieceIdx = 0; PieceIdx < mPieces.GetSize(); PieceIdx++)
+	{
+		lcPiece* Piece = mPieces[PieceIdx];
+
+		if (!Piece->IsSelected() && Piece->IsVisible(mCurrentStep) && (Piece->GetTopGroup() == TopGroup))
+			Piece->SetSelected(Select);
+	}
+}
+
+void lcModel::FocusOrDeselectObject(const lcObjectSection& ObjectSection)
+{
+	lcObject* FocusObject = GetFocusObject();
+	lcObject* Object = ObjectSection.Object;
+	lcuint32 Section = ObjectSection.Section;
+
+	if (Object)
+	{
+		bool WasSelected = Object->IsSelected();
+
+		if (!Object->IsFocused(Section))
+		{
+			if (FocusObject)
+				FocusObject->SetFocused(FocusObject->GetFocusSection(), false);
+
+			Object->SetFocused(Section, true);
+		}
+		else
+			Object->SetSelected(Section, false);
+
+		bool IsSelected = Object->IsSelected();
+
+		if (Object->IsPiece() && (WasSelected != IsSelected))
+			SelectGroup(((lcPiece*)Object)->GetTopGroup(), IsSelected);
+	}
+	else
+	{
+		if (FocusObject)
+			FocusObject->SetFocused(FocusObject->GetFocusSection(), false);
+	}
+
+	UpdateSelection();
+	gMainWindow->UpdateAllViews();
+	gMainWindow->UpdateFocusObject(GetFocusObject());
+}
+
+void lcModel::ClearSelectionAndSetFocus(lcObject* Object, lcuint32 Section)
+{
+	ClearSelection(false);
+
+	if (Object)
+	{
+		Object->SetFocused(Section, true);
+
+		if (Object->IsPiece())
+			SelectGroup(((lcPiece*)Object)->GetTopGroup(), true);
+	}
+
+	UpdateSelection();
+	gMainWindow->UpdateAllViews();
+	gMainWindow->UpdateFocusObject(Object);
+}
+
+void lcModel::ClearSelectionAndSetFocus(const lcObjectSection& ObjectSection)
+{
+	ClearSelectionAndSetFocus(ObjectSection.Object, ObjectSection.Section);
+}
+
+void lcModel::SetSelection(const lcArray<lcObjectSection>& ObjectSections)
+{
+	ClearSelection(false);
+	AddToSelection(ObjectSections);
+}
+
+void lcModel::AddToSelection(const lcArray<lcObjectSection>& ObjectSections)
+{
+	for (int ObjectIdx = 0; ObjectIdx < ObjectSections.GetSize(); ObjectIdx++)
+	{
+		lcObject* Object = ObjectSections[ObjectIdx].Object;
+
+		bool WasSelected = Object->IsSelected();
+		Object->SetSelected(ObjectSections[ObjectIdx].Section, true);
+
+		if (!WasSelected && Object->GetType() == LC_OBJECT_PIECE)
+			SelectGroup(((lcPiece*)Object)->GetTopGroup(), true);
+	}
+
+	UpdateSelection();
+	gMainWindow->UpdateAllViews();
+	gMainWindow->UpdateFocusObject(GetFocusObject());
+}

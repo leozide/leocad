c1abbddca2a07907c0a122f41dc10c2c90546724
leo <leozide@users.noreply.github.com>
2014-09-08 22:01:24 +0000

    Added space between string literals to fix errors in c++11.

--- common/lc_application.cpp
+++ common/lc_application.cpp
@@ -212,7 +212,7 @@ bool lcApplication::Initialize(int argc, char* argv[], const char* LibraryInstal
 			else if ((strcmp(Param, "-v") == 0) || (strcmp(Param, "--version") == 0))
 			{
 				printf("LeoCAD Version " LC_VERSION_TEXT "\n");
-				printf("Compiled "__DATE__"\n");
+				printf("Compiled " __DATE__ "\n");
 
 				return false;
 			}

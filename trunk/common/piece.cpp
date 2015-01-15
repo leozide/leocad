be748f1915d34fa26dcb615c5e5fc048d83cd3c6
leo <leo@c7d43263-9d01-0410-8a33-9dba5d9f93d6>
2015-01-15 02:21:10 +0000

    Removed support for naming pieces in a model.

--- trunk/common/piece.cpp
+++ trunk/common/piece.cpp
@@ -30,7 +30,6 @@ lcPiece::lcPiece(PieceInfo* pPieceInfo)
 	mColorCode = 16;
 	mStepShow = 1;
 	mStepHide = LC_STEP_MAX;
-	memset(m_strName, 0, sizeof(m_strName));
 	mGroup = NULL;
 
 	if (mPieceInfo != NULL)
@@ -52,35 +51,6 @@ void lcPiece::SaveLDraw(QTextStream& Stream) const
 
 	if (mStepHide != LC_STEP_MAX)
 		Stream << QLatin1String("0 !LEOCAD PIECE STEP_HIDE ") << mStepHide << LineEnding;
-
-	if (m_strName[0])
-	{
-		int Length = strlen(mPieceInfo->m_strDescription);
-		bool Save = true;
-
-		if (!strncmp(m_strName, mPieceInfo->m_strDescription, Length))
-		{
-			const char* Name = m_strName + Length;
-
-			if (*Name++ == ' ' && *Name++ == '#')
-			{
-				for (;;)
-				{
-					if (!isdigit(*Name++))
-						break;
-
-					if (*Name == 0)
-					{
-						Save = false;
-						break;
-					}
-				}
-			}
-		}
-
-		if (Save)
-			Stream << QLatin1String("0 !LEOCAD PIECE NAME ") << m_strName << LineEnding;
-	}
 
 	if (IsHidden())
 		Stream << QLatin1String("0 !LEOCAD PIECE HIDDEN") << LineEnding;
@@ -124,13 +94,6 @@ bool lcPiece::ParseLDrawLine(QTextStream& Stream)
 
 		if (Token == QLatin1String("STEP_HIDE"))
 			Stream >> mStepHide;
-		else if (Token == QLatin1String("NAME"))
-		{
-			QString Name = Stream.readAll().trimmed();
-			QByteArray NameUtf = Name.toUtf8(); // todo: replace with qstring
-			strncpy(m_strName, NameUtf.constData(), sizeof(m_strName));
-			m_strName[sizeof(m_strName) - 1] = 0;
-		}
 		else if (Token == QLatin1String("HIDDEN"))
 			SetHidden(true);
 		else if (Token == QLatin1String("POSITION_KEY"))
@@ -326,7 +289,7 @@ bool lcPiece::FileLoad(lcFile& file)
 	  if (Hidden & 1)
 		  mState |= LC_PIECE_HIDDEN;
       file.ReadU8(&ch, 1);
-      file.ReadBuffer(m_strName, ch);
+	  file.Seek(ch, SEEK_CUR);
     }
     else
     {
@@ -334,7 +297,7 @@ bool lcPiece::FileLoad(lcFile& file)
       file.ReadS32(&hide, 1);
       if (hide != 0)
         mState |= LC_PIECE_HIDDEN;
-      file.ReadBuffer(m_strName, 81);
+	  file.Seek(81, SEEK_CUR);
     }
 
     // 7 (0.64)
@@ -373,43 +336,6 @@ void lcPiece::Initialize(const lcMatrix44& WorldMatrix, lcStep Step)
 	ChangeKey(mRotationKeys, lcMatrix33(WorldMatrix), 1, true);
 
 	UpdatePosition(Step);
-}
-
-void lcPiece::CreateName(const lcArray<lcPiece*>& Pieces)
-{
-	if (m_strName[0])
-	{
-		bool Found = false;
-
-		for (int PieceIdx = 0; PieceIdx < Pieces.GetSize(); PieceIdx++)
-		{
-			if (!strcmp(Pieces[PieceIdx]->m_strName, m_strName))
-			{
-				Found = true;
-				break;
-			}
-		}
-
-		if (!Found)
-			return;
-	}
-
-	const char* Prefix = mPieceInfo->m_strDescription;
-	int Length = strlen(Prefix);
-	int i, max = 0;
-
-	for (int PieceIdx = 0; PieceIdx < Pieces.GetSize(); PieceIdx++)
-	{
-		lcPiece* Piece = Pieces[PieceIdx];
-
-		if (strncmp(Piece->m_strName, Prefix, Length) == 0)
-			if (sscanf(Piece->m_strName + Length, " #%d", &i) == 1)
-				if (i > max)
-					max = i;
-	}
-
-	snprintf(m_strName, sizeof(m_strName), "%s #%.2d", Prefix, max + 1);
-	m_strName[sizeof(m_strName) - 1] = 0;
 }
 
 void lcPiece::InsertTime(lcStep Start, lcStep Time)
@@ -555,6 +481,11 @@ void lcPiece::Move(lcStep Step, bool AddKey, const lcVector3& Distance)
 	ChangeKey(mPositionKeys, Position, Step, AddKey);
 
 	mModelWorld.SetTranslation(Position);
+}
+
+const char* lcPiece::GetName() const
+{
+	return mPieceInfo->m_strDescription;
 }
 
 bool lcPiece::IsVisible(lcStep Step)

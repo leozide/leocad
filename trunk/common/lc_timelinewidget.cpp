35e5afccd7f4b10ad7ea9cc868faf502b9b278a8
leo <leo@c7d43263-9d01-0410-8a33-9dba5d9f93d6>
2015-03-13 05:26:01 +0000

    Made the timeline selection match the current model selection.

--- trunk/common/lc_timelinewidget.cpp
+++ trunk/common/lc_timelinewidget.cpp
@@ -4,39 +4,68 @@
 #include "project.h"
 #include "piece.h"
 #include "pieceinf.h"
+#include "lc_mainwindow.h"
 
 lcTimelineWidget::lcTimelineWidget(QWidget* Parent)
 	: QTreeWidget(Parent)
 {
+	mIgnoreUpdates = false;
+
 	setSelectionMode(QAbstractItemView::ExtendedSelection);
 	setDragEnabled(true);
 	setDragDropMode(QAbstractItemView::InternalMove);
 	setUniformRowHeights(true);
 	setHeaderHidden(true);
+	setContextMenuPolicy(Qt::CustomContextMenu);
+
+	connect(this, SIGNAL(itemSelectionChanged()), SLOT(ItemSelectionChanged()));
+	connect(this, SIGNAL(customContextMenuRequested(QPoint)), SLOT(CustomMenuRequested(QPoint)));
 }
 
 lcTimelineWidget::~lcTimelineWidget()
 {
 }
 
+void lcTimelineWidget::CustomMenuRequested(QPoint Pos)
+{
+	QList<QTreeWidgetItem*> SelectedItems = selectedItems();
+
+	if (!SelectedItems.isEmpty())
+	{
+		QMenu* Menu = new QMenu(this);
+
+		Menu->addAction(gMainWindow->mActions[LC_PIECE_HIDE_SELECTED]);
+		Menu->addAction(gMainWindow->mActions[LC_PIECE_HIDE_UNSELECTED]);
+		Menu->addAction(gMainWindow->mActions[LC_PIECE_UNHIDE_ALL]);
+
+		Menu->popup(viewport()->mapToGlobal(Pos));
+	}
+}
+
 void lcTimelineWidget::Update()
 {
-	clear();
-
 	lcModel* Model = lcGetActiveModel();
 
 	if (!Model)
+	{
+		mItems.clear();
+		clear();
 		return;
+	}
+
+	bool Blocked = blockSignals(true);
 
 	int Steps = Model->GetLastStep();
-	QList<QTreeWidgetItem*> StepItems;
-	StepItems.reserve(Steps);
 
-	for (int Step = 0; Step < Steps; Step++)
+	for (int TopLevelItemIdx = Steps; TopLevelItemIdx < topLevelItemCount(); TopLevelItemIdx++)
+		delete topLevelItem(TopLevelItemIdx);
+
+	for (int Step = topLevelItemCount(); Step < Steps; Step++)
 	{
 		QTreeWidgetItem* StepItem = new QTreeWidgetItem(this, QStringList(tr("Step %1").arg(Step + 1)));
 		StepItem->setFlags(Qt::ItemIsEnabled | Qt::ItemIsDropEnabled);
-		StepItems.append(StepItem);
+		addTopLevelItem(StepItem);
+		StepItem->setExpanded(true);
 	}
 
 	const lcArray<lcPiece*>& Pieces = Model->GetPieces();
@@ -44,13 +73,67 @@ void lcTimelineWidget::Update()
 	for (int PieceIdx = 0; PieceIdx < Pieces.GetSize(); PieceIdx++)
 	{
 		lcPiece* Piece = Pieces[PieceIdx];
-		QTreeWidgetItem* PieceItem = new QTreeWidgetItem(StepItems[Piece->GetStepShow() - 1], QStringList(Piece->mPieceInfo->m_strDescription));
-		PieceItem->setFlags(Qt::ItemIsSelectable | Qt::ItemIsEnabled | Qt::ItemIsDragEnabled);
-		PieceItem->setData(0, Qt::UserRole, qVariantFromValue<uintptr_t>((uintptr_t)Piece));
+		QTreeWidgetItem* PieceItem = mItems.value(Piece);
+		QTreeWidgetItem* StepItem = topLevelItem(Piece->GetStepShow() - 1);
+
+		if (PieceItem)
+		{
+			if (PieceItem->parent() != StepItem)
+				StepItem->addChild(PieceItem);
+		}
+		else
+		{
+			PieceItem = new QTreeWidgetItem(StepItem, QStringList(Piece->mPieceInfo->m_strDescription));
+			PieceItem->setFlags(Qt::ItemIsSelectable | Qt::ItemIsEnabled | Qt::ItemIsDragEnabled);
+			PieceItem->setData(0, Qt::UserRole, qVariantFromValue<uintptr_t>((uintptr_t)Piece));
+			StepItem->addChild(PieceItem);
+			mItems[Piece] = PieceItem;
+		}
+
+		PieceItem->setSelected(Piece->IsSelected());
 	}
 
-	insertTopLevelItems(0, StepItems);
-	expandAll();
+	blockSignals(Blocked);
+}
+
+void lcTimelineWidget::UpdateSelection()
+{
+	if (mIgnoreUpdates)
+		return;
+
+	bool Blocked = blockSignals(true);
+
+	for (int TopLevelItemIdx = 0; TopLevelItemIdx < topLevelItemCount(); TopLevelItemIdx++)
+	{
+		QTreeWidgetItem* StepItem = topLevelItem(TopLevelItemIdx);
+
+		for (int PieceItemIdx = 0; PieceItemIdx < StepItem->childCount(); PieceItemIdx++)
+		{
+			QTreeWidgetItem* PieceItem = StepItem->child(PieceItemIdx);
+			lcPiece* Piece = (lcPiece*)PieceItem->data(0, Qt::UserRole).value<uintptr_t>();
+
+			PieceItem->setSelected(Piece->IsSelected());
+		}
+	}
+
+	blockSignals(Blocked);
+}
+
+void lcTimelineWidget::ItemSelectionChanged()
+{
+	lcArray<lcObject*> Selection;
+
+	foreach (QTreeWidgetItem* PieceItem, selectedItems())
+	{
+		lcPiece* Piece = (lcPiece*)PieceItem->data(0, Qt::UserRole).value<uintptr_t>();
+		Selection.Add(Piece);
+	}
+
+	bool Blocked = blockSignals(true);
+	mIgnoreUpdates = true;
+	lcGetActiveModel()->SetSelection(Selection);
+	mIgnoreUpdates = false;
+	blockSignals(Blocked);
 }
 
 void lcTimelineWidget::dropEvent(QDropEvent* Event)
--- trunk/common/lc_timelinewidget.h
+++ trunk/common/lc_timelinewidget.h
@@ -3,14 +3,24 @@
 
 class lcTimelineWidget : public QTreeWidget
 {
+	Q_OBJECT
+
 public:
 	lcTimelineWidget(QWidget* Parent);
 	virtual ~lcTimelineWidget();
 
 	void Update();
+	void UpdateSelection();
+
+public slots:
+	void ItemSelectionChanged();
+	void CustomMenuRequested(QPoint Pos);
 
 protected:
 	virtual void dropEvent(QDropEvent* Event);
+
+	QMap<lcPiece*, QTreeWidgetItem*> mItems;
+	bool mIgnoreUpdates;
 };
 
 #endif // _LC_TIMELINEWIDGET_H_

c5f4500b4b07d1e5651d1368022af7ae002b6149
Leonardo Zide <leozide@gmail.com>
2018-02-25 15:58:16 -0800

    Disabled LC_LOAD_EXTENSIONS for GLES.

--- common/lc_glextensions.h
+++ common/lc_glextensions.h
@@ -10,7 +10,7 @@ extern bool gSupportsTexImage2DMultisample;
 extern bool gSupportsAnisotropic;
 extern GLfloat gMaxAnisotropy;
 
-#ifndef Q_OS_MAC
+#if !defined(Q_OS_MAC) && !defined(QT_OPENGL_ES)
 #define LC_LOAD_GLEXTENSIONS
 #endif
 

8174cc230670e12a64cd5401903d5672e57042e9
jschwender <joachim.schwender@web.de>
2018-02-28 19:59:29 +0100

    Update leocad_de.ts

--- resources/leocad_de.ts
+++ resources/leocad_de.ts
@@ -3900,7 +3900,7 @@ Bitte lesen sie unter http://www.leocad.org nach wie man eine Bibliothek herunte
     <message numerus="yes">
         <location filename="../common/lc_mainwindow.cpp" line="1820"/>
         <source>%n Object(s) selected</source>
-        <translation>%n Objekte ausgewählt</translation>
+        <translation>Objekte ausgewählt</translation>
     </message>
     <message>
         <location filename="../common/lc_mainwindow.cpp" line="2143"/>

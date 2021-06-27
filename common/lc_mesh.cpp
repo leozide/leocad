5decefcb731a5199306ba8d9fb5f43d9efb9e31f
Leonardo Zide <leozide@gmail.com>
2021-06-27 12:44:05 -0700

    Crash fix.

--- common/lc_mesh.cpp
+++ common/lc_mesh.cpp
@@ -424,7 +424,9 @@ bool lcMesh::FileLoad(lcMemFile& File)
 				FileName[Length] = 0;
 
 				Section.Texture = lcGetPiecesLibrary()->FindTexture(FileName, nullptr, false);
-				Section.Texture->AddRef();
+
+				if (Section.Texture)
+					Section.Texture->AddRef();
 			}
 			else
 				Section.Texture = nullptr;

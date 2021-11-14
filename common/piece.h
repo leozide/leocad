5d558b38bba9425e1b21965f9362b91631d9782b
Leonardo Zide <leozide@gmail.com>
2021-11-13 17:52:29 -0800

    Warning fix.

--- common/piece.h
+++ common/piece.h
@@ -54,6 +54,8 @@ public:
 
 	void SetSelected(quint32 Section, bool Selected) override
 	{
+		Q_UNUSED(Section);
+
 		mSelected = Selected;
 		if (!Selected)
 			mFocusedSection = LC_PIECE_SECTION_INVALID;

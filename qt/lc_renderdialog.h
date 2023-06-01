f4da046f81490337ab11fde546e3b7daac714f6b
Trevor SANDY <trevor.sandy@gmail.com>
2023-06-02 00:02:53 +0200

    Blender addon - render dialog

--- qt/lc_renderdialog.h
+++ qt/lc_renderdialog.h
@@ -6,6 +6,19 @@ namespace Ui {
 class lcRenderDialog;
 }
 
+class QProcess;
+class RenderProcess : public QProcess
+{
+	Q_OBJECT
+
+public:
+	explicit RenderProcess(QObject* parent = nullptr)
+		: QProcess(parent)
+	{
+	}
+	~RenderProcess();
+};
+
 class lcRenderPreviewWidget : public QWidget
 {
 	Q_OBJECT
@@ -36,34 +49,60 @@ class lcRenderDialog : public QDialog
 	Q_OBJECT
 
 public:
-	explicit lcRenderDialog(QWidget* Parent);
+	explicit lcRenderDialog(QWidget* Parent, int Command);
 	~lcRenderDialog();
 
 public slots:
 	void reject() override;
 	void on_RenderButton_clicked();
 	void on_OutputBrowseButton_clicked();
+	void on_RenderSettingsButton_clicked();
+	void on_RenderOutputButton_clicked();
 	void Update();
 
 protected slots:
-	void ReadStdErr();
-	void WriteStdLog(bool = false);
+	void ReadStdOut();
+	void WriteStdOut();
+	void UpdateElapsedTime() const;
 
 protected:
-	QString GetOutputFileName() const;
+	QString GetOutputFileName(int StdErr = 0) const;
 	QString GetPOVFileName() const;
+	QString ReadStdErr(bool& Error) const;
 	void CloseProcess();
 	bool PromptCancel();
 	void ShowResult();
-
-#ifndef QT_NO_PROCESS
-	QProcess* mProcess;
+#ifdef Q_OS_WIN
+	int TerminateChildProcess(const qint64 Pid, const qint64 Ppid);
 #endif
+#ifndef QT_NO_PROCESS
+	RenderProcess* mProcess;
+#endif
+	enum CommandType
+	{
+		POVRAY_RENDER,
+		BLENDER_RENDER,
+		OPEN_IN_BLENDER
+	};
+
 	QTimer mUpdateTimer;
+	QElapsedTimer mRenderTime;
 	QFile mOutputFile;
 	void* mOutputBuffer;
 	QImage mImage;
 	QStringList mStdErrList;
+	QStringList mStdOutList;
+
+	int mWidth;
+	int mHeight;
+	int mPreviewWidth;
+	int mPreviewHeight;
+	int mCommand;
+	int mBlendProgValue;
+	int mBlendProgMax;
+	double mScale;
+	QString mImportModule;
+	QString mDataPath;
 
 	Ui::lcRenderDialog* ui;
 };

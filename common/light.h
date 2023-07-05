9cc97c9a6cce6c0467cc98682ac329554eeca700
Trevor SANDY <trevor.sandy@gmail.com>
2023-07-05 07:14:43 +0200

    Blender lights - lcLight

--- common/light.h
+++ common/light.h
@@ -21,11 +21,53 @@ enum lcLightSection
 	LC_LIGHT_SECTION_TARGET
 };
 
+enum lcLightType
+{
+	LC_UNDEFINED_LIGHT,
+	LC_POINTLIGHT,
+	LC_AREALIGHT,
+	LC_SUNLIGHT,
+	LC_SPOTLIGHT,
+	LC_NUM_LIGHT_TYPES
+};
+
+enum lcLightProperty
+{
+	LC_UNDEFINED_SHAPE = -1,
+	LC_LIGHT_NONE,
+	LC_LIGHT_SHAPE_SQUARE = LC_LIGHT_NONE,
+	LC_LIGHT_SHAPE,
+	LC_LIGHT_SHAPE_DISK = LC_LIGHT_SHAPE,
+	LC_LIGHT_COLOR,
+	LC_LIGHT_SHAPE_RECTANGLE = LC_LIGHT_COLOR,
+	LC_LIGHT_TYPE,
+	LC_LIGHT_SHAPE_ELLIPSE = LC_LIGHT_TYPE,
+	LC_LIGHT_FACTOR,
+	LC_LIGHT_SPECULAR,
+	LC_LIGHT_EXPONENT,
+	LC_LIGHT_SPOT_SIZE,
+	LC_LIGHT_CUTOFF,
+	LC_LIGHT_USE_CUTOFF
+};
+
+class lcLightProps
+{
+public:
+	lcVector3 mLightColor;
+	lcVector2 mLightFactor;
+	float     mLightSpecular;
+	float     mSpotExponent;
+	float     mSpotCutoff;
+	float     mSpotSize;
+	bool      mEnableCutoff;
+	int       mLightShape;
+};
+
 class lcLight : public lcObject
 {
 public:
 	lcLight(float px, float py, float pz);
-	lcLight(float px, float py, float pz, float tx, float ty, float tz);
+	lcLight(float px, float py, float pz, float tx, float ty, float tz, int LightType);
 	~lcLight();
 
 	lcLight(const lcLight&) = delete;
@@ -178,6 +220,7 @@ public:
 	}
 
 	void SaveLDraw(QTextStream& Stream) const;
+	bool ParseLDrawLine(QTextStream& Stream);
 
 public:
 	void RayTest(lcObjectRayTest& ObjectRayTest) const override;
@@ -191,6 +234,11 @@ public:
 	bool IsVisible() const
 	{ return (mState & LC_LIGHT_HIDDEN) == 0; }
 
+	void SetName(const QString& Name)
+	{
+		mName = Name;
+	}
+
 	QString GetName() const override
 	{
 		return mName;
@@ -201,6 +249,20 @@ public:
 	void MoveSelected(lcStep Step, bool AddKey, const lcVector3& Distance);
 	bool Setup(int LightIndex);
 	void CreateName(const lcArray<lcLight*>& Lights);
+	void UpdateLight(lcStep Step, lcLightProps Props, int Property);
+	lcLightProps GetLightProps() const
+	{
+		lcLightProps props;
+		props.mLightColor = mLightColor;
+		props.mLightFactor = mLightFactor;
+		props.mLightSpecular = mLightSpecular;
+		props.mSpotExponent = mSpotExponent;
+		props.mSpotCutoff = mSpotCutoff;
+		props.mSpotSize = mSpotSize;
+		props.mEnableCutoff = mEnableCutoff;
+		props.mLightShape = mLightShape;
+		return props;
+	}
 
 	// Temporary parameters
 	lcMatrix44 mWorldLight;
@@ -210,8 +272,20 @@ public:
 	lcVector4 mDiffuseColor;
 	lcVector4 mSpecularColor;
 	lcVector3 mAttenuation;
+	lcVector3 mLightColor;
+	lcVector2 mLightFactor;
+	bool  mAngleSet;
+	bool  mSpotBlendSet;
+	bool  mSpotCutoffSet;
+	bool  mHeightSet;
+	bool  mEnableCutoff;
+	int   mLightType;
+	int   mLightShape;
+	float mLightSpecular;
+	float mSpotSize;
 	float mSpotCutoff;
 	float mSpotExponent;
+	QString mName;
 
 protected:
 	lcObjectKeyArray<lcVector3> mPositionKeys;
@@ -220,14 +294,21 @@ protected:
 	lcObjectKeyArray<lcVector4> mDiffuseColorKeys;
 	lcObjectKeyArray<lcVector4> mSpecularColorKeys;
 	lcObjectKeyArray<lcVector3> mAttenuationKeys;
+	lcObjectKeyArray<lcVector3> mLightColorKeys;
+	lcObjectKeyArray<lcVector2> mLightFactorKeys;
+	lcObjectKeyArray<int> mLightTypeKeys;
+	lcObjectKeyArray<int> mLightShapeKeys;
+	lcObjectKeyArray<float> mLightSpecularKeys;
+	lcObjectKeyArray<float> mLightSpotSizeKeys;
 	lcObjectKeyArray<float> mSpotCutoffKeys;
 	lcObjectKeyArray<float> mSpotExponentKeys;
 
-	void Initialize(const lcVector3& Position, const lcVector3& TargetPosition);
+	void Initialize(const lcVector3& Position, const lcVector3& TargetPosition, int LightType);
 
+	void DrawDirectionalLight(lcContext* Context) const;
 	void DrawPointLight(lcContext* Context) const;
 	void DrawSpotLight(lcContext* Context) const;
+	void SetLightState(int LightType);
 
-	QString mName;
 	quint32 mState;
 };

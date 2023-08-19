410bdb2bb607c87ff7e5333ba8ddf8d081d7e347
Trevor SANDY <trevor.sandy@gmail.com>
2023-08-19 17:33:57 +0200

    Blender addon - fix chosen logo options

--- common/lc_blenderpreferences.cpp
+++ common/lc_blenderpreferences.cpp
@@ -250,7 +250,7 @@ lcBlenderPreferences::BlenderSettings  lcBlenderPreferences::mDefaultSettingsMM 
 lcBlenderPreferences::ComboItems  lcBlenderPreferences::mComboItemsMM [NUM_COMBO_ITEMS_MM] =
 {
 	/*    FIRST item set as default        Data                                  Item: */
-	/* 00 LBL_CHOSEN_LOGO              */ {"logo|logo2|logo3",                   QObject::tr("Logo (Not Used)|Logo2 (Not Used)|Flattened(3)")},
+	/* 00 LBL_CHOSEN_LOGO              */ {"logo3|logo4|logo5",                  QObject::tr("Raised flattened logo geometry(3)|Raised rounded logo geometry(4)|Subtle rounded logo geometry(5)")},
 	/* 01 LBL_COLOUR_SCHEME_MM         */ {"lgeo|ldraw|alt|custom",              QObject::tr("Realistic Colours|Original LDraw Colours|Alternate LDraw Colours|Custom Colours")},
 	/* 02 LBL_COLOUR_STRATEGY          */ {"material|vertex_colors",             QObject::tr("Material|Vertex Colors")},
 	/* 03 LBL_GAP_SCALE_STRATEGY       */ {"object|constraint",                  QObject::tr("Gap applied directly to object|Gap scaled to adjust to gaps between parts")},
